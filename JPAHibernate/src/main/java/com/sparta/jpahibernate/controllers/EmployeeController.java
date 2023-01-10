6845618d595a871f1ef5b2c2747bedcce7ffaac7
yash jagani <shi7su@protonmail.ch>
2023-01-10 17:50:39 +0000

    Changes by Emre

--- JPAHibernate/src/main/java/com/sparta/jpahibernate/controllers/EmployeeController.java
+++ JPAHibernate/src/main/java/com/sparta/jpahibernate/controllers/EmployeeController.java
@@ -25,4 +25,57 @@ public class EmployeeController {
         model.addAttribute("employee", employee);
         return "employeeDisplay";
     }
+
+    @GetMapping("/all")
+    public String getAllEmployees(Model model) {
+        List<EmployeeDTO> employees = employeeDAO.findAll();
+        model.addAttribute("employees", employees);
+        return "employeeDisplayAll";
+    }
+
+    @GetMapping ("/new")
+    public String createEmployee(Model model){
+        EmployeeDTO employee = new EmployeeDTO();
+        model.addAttribute("employee", employee);
+        return "employeeCreate";
+    }
+
+    @PostMapping("/new/success")
+    public String createEmployeeSuccess(@ModelAttribute("employee")EmployeeDTO employee, Model model){
+        employeeDAO.save(employee);
+        model.addAttribute("employee", employee);
+        return "employeeCreateSuccess";
+    }
+
+    @GetMapping("/delete")
+    public String deleteEmployee(Model model){
+        model.addAttribute("employee", new EmployeeDTO());
+        return "employeeDelete";
+    }
+
+    @PostMapping("/delete/success")
+    public String deleteEmployeeSuccess(@ModelAttribute("employee")EmployeeDTO employee, Model model){
+        if(employeeDAO.findById(employee.getId()).isEmpty()) {
+            employee = null;
+        } else {
+            employeeDAO.delete(employee);
+        }
+        model.addAttribute("employee", employee);
+        return "employeeDeleteSuccess";
+    }
+
+    @GetMapping("/update/{id}")
+    public String showUpdateForm(@PathVariable("id") int id, Model model) {
+        EmployeeDTO employee = employeeDAO.findById(id)
+                .orElse(null);
+        model.addAttribute("employee", employee);
+        return "employeeUpdate";
+    }
+
+    @PostMapping("/update/success")
+    public String updateEmployee(@ModelAttribute("employee") EmployeeDTO employee, Model model) {
+        employeeDAO.save(employee);
+        model.addAttribute("employee", employee);
+        return "employeeUpdateSuccess";
+    }
 }
