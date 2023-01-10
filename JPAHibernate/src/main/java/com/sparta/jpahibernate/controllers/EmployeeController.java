5903ebf945d03780fa9e8fdf45ad9f6bf0071b3b
yash jagani <shi7su@protonmail.ch>
2023-01-10 17:50:07 +0000

    crud, dept analytical

--- JPAHibernate/src/main/java/com/sparta/jpahibernate/controllers/EmployeeController.java
+++ JPAHibernate/src/main/java/com/sparta/jpahibernate/controllers/EmployeeController.java
@@ -1,159 +1,28 @@
 package com.sparta.jpahibernate.controllers;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.json.JsonMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.sparta.jpahibernate.dao.concretes.DepartmentDAOImpl;
 import com.sparta.jpahibernate.dao.concretes.EmployeeDAOImpl;
-import com.sparta.jpahibernate.dao.concretes.UserDAOImpl;
+import com.sparta.jpahibernate.dto.DepartmentDTO;
 import com.sparta.jpahibernate.dto.EmployeeDTO;
-import com.sparta.jpahibernate.dto.EmpsByDeptsDTO;
+import com.sparta.jpahibernate.repositories.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
-import java.time.LocalDate;
 import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
-import java.util.stream.Stream;
 
-@RestController
-@RequestMapping("/api/employees")
+@Controller
+@RequestMapping("/web/employee")
 public class EmployeeController {
 
     @Autowired
-    private EmployeeDAOImpl empDao;
-
-    @Autowired
-    private UserDAOImpl userDao;
-
-    @PostMapping("/add")
-    public ResponseEntity<String> addNewEmployee(@RequestBody EmployeeDTO newEmployee, @RequestParam String apiKey){
-        if (userDao.isAdmin(apiKey) || userDao.isUpdate(apiKey)){
-            Optional<EmployeeDTO> original = empDao.findById(newEmployee.getId());
-            HttpHeaders headers = new HttpHeaders();
-            headers.add("content-type", "application/json");
-            if (original.isPresent()) {
-                throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
-                        "Employee with id: " + newEmployee.getId() + " already exists");
-            } else if (Stream.of(newEmployee.getId(), newEmployee.getBirthDate(), newEmployee.getFirstName(),
-                            newEmployee.getLastName(), newEmployee.getGender(), newEmployee.getHireDate())
-                    .anyMatch(Objects::isNull)) {
-                throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
-                        "The employee cannot be null");
-            }
-            empDao.save(newEmployee);
-            return new ResponseEntity<>(newEmployee.toString(), headers, HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>("\"message\":\"User could not be added\"}",
-                    HttpStatus.UNAUTHORIZED);
-        }
-
-    }
-
-    @PutMapping("/update/create")
-    public ResponseEntity<EmployeeDTO> updateEmployeePut(@RequestBody EmployeeDTO newState, @RequestParam String apiKey){
-        if (userDao.isAdmin(apiKey) || userDao.isUpdate(apiKey)) {
-            Optional<EmployeeDTO> original = empDao.findById(newState.getId());
-            if (original.isPresent()) {
-                String[] params = new String[]{newState.getId().toString(), newState.getBirthDate().toString(), newState.getFirstName(), newState.getLastName(), newState.getGender(), newState.getHireDate().toString()};
-                empDao.update(newState.getId(), params);
-                return new ResponseEntity<>(empDao.findById(newState.getId()).get(), HttpStatus.OK);
-            } else {
-                empDao.save(newState);
-                return new ResponseEntity<>(empDao.findById(newState.getId()).get(), HttpStatus.OK);
-            }
-        }
-        return new ResponseEntity<>(null, HttpStatus.UNAUTHORIZED);
-    }
-
-    @PatchMapping("/update")
-    public ResponseEntity<EmployeeDTO> updateEmployeePatch(@RequestBody EmployeeDTO newState, @RequestParam String apiKey){
-        if (userDao.isAdmin(apiKey) || userDao.isUpdate(apiKey)) {
-            String[] params = new String[]{newState.getId().toString(), newState.getBirthDate().toString(), newState.getFirstName(), newState.getLastName(), newState.getGender(), newState.getHireDate().toString()};
-            empDao.update(newState.getId(), params);
-            System.out.println(empDao.findById(newState.getId()).get());
-            return new ResponseEntity<>(empDao.findById(newState.getId()).get(), HttpStatus.OK);
-        }
-        return new ResponseEntity<>(null, HttpStatus.UNAUTHORIZED);
-    }
+    private EmployeeDAOImpl employeeDAO;
 
     @GetMapping("/{id}")
-    public EmployeeDTO findDepartmentById(@PathVariable int id) {
-        return empDao.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
-                "No employee with the specified ID could be found"));
-    }
-
-//    @GetMapping("/{id}")
-//    public ResponseEntity<?> findById(@PathVariable int id) {
-//        Optional<EmployeeDTO> employee = empDao.findById(id);
-//        ObjectMapper empMap = JsonMapper.builder()
-//                .addModule(new JavaTimeModule())
-//                .build();
-//        HttpHeaders headers = new HttpHeaders();
-//        if(employee.isPresent()){
-//            EmployeeDTO emp = employee.get();
-//            String empString;
-//            try {
-//                empString = empMap.writeValueAsString(emp);
-//            } catch (JsonProcessingException e){
-//                throw new RuntimeException(e);
-//            }
-//            return new ResponseEntity<>(empString, headers, HttpStatus.OK);
-//        }
-//        return new ResponseEntity<>("{\"message\":\"Employee with id: " + id + " was not found\"}",
-//        headers, HttpStatus.NOT_FOUND);
-//    }
-
-    @DeleteMapping("/delete/{id}")
-    public void deleteById(@PathVariable int id, @RequestParam String apiKey){
-        if (userDao.isAdmin(apiKey)) {
-            empDao.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
-                    "No employee with the specific ID could be found"));
-            empDao.deleteById(id);
-        }
-    }
-
-//    @DeleteMapping("/{id}")
-//    public ResponseEntity<String> deleteById(@PathVariable int id){
-//        Optional <EmployeeDTO> emp = empDao.findById(id);
-//        HttpHeaders headers = new HttpHeaders();
-//        headers.add("content-type", "application/json");
-//        if(emp.isPresent()){
-//            empDao.deleteById(empDao.findById(id).get().getId());
-//            return new ResponseEntity<>("{\"message\":\"Employee " + id+ " successfully deleted\"},", headers, HttpStatus.OK);
-//        } else {
-//            return new ResponseEntity<>("{\"message\":\"Employee " + id+ " not found\"},", headers, HttpStatus.NOT_FOUND);
-//        }
-//    }
-
-    @GetMapping("/countNumberOfEmployeesLeftDepartmentByYear")
-    public int countNumberOfEmployeesLeftDepartmentByYear(@RequestParam("dep") String dep,
-                                                          @RequestParam("year") String year,
-                                                          @RequestParam String apiKey){
-
-        System.out.println( "In the year:" + year + "the number of employees left in department:" + dep );
-        int res = empDao.countNumberOfEmployeesLeftDepartmentByYear(dep,year);
-        System.out.println(res);
-        return res;
-    }
-
-    @GetMapping("/findByDepartmentAndDate")
-    public List<EmpsByDeptsDTO> findByDepartmentAndDate(@RequestParam("dep") String dep,
-                                                        @RequestParam("fromDate") LocalDate fromDate,
-                                                        @RequestParam("toDate") LocalDate toDate,
-                                                        @RequestParam String apiKey){
-
-        return empDao.findByDepartmentAndDate(dep, fromDate, toDate);
-    }
-
-    @GetMapping("/findByLastName")
-    public List<EmployeeDTO> findByLastName(@RequestParam("lastName") String lastName,
-                                            @RequestParam String apiKey){
-        return empDao.findByLastName(lastName);
+    public String getEmployee(@PathVariable("id") int id, Model model) {
+        EmployeeDTO employee = employeeDAO.findById(id).orElse(null);
+        model.addAttribute("employee", employee);
+        return "employeeDisplay";
     }
 }
--- JPAHibernate/src/main/java/com/sparta/jpahibernate/controllers/UserController.java
+++ JPAHibernate/src/main/java/com/sparta/jpahibernate/controllers/UserController.java
@@ -1,104 +1,33 @@
 package com.sparta.jpahibernate.controllers;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.json.JsonMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.sparta.jpahibernate.dao.concretes.UserDAOImpl;
-import com.sparta.jpahibernate.dto.LoginDTO;
+import com.sparta.jpahibernate.dao.interfaces.UserDAO;
+import com.sparta.jpahibernate.dto.DepartmentDTO;
 import com.sparta.jpahibernate.dto.UserDTO;
-import com.sparta.jpahibernate.entities.User;
+import com.sparta.jpahibernate.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.ui.Model;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.Optional;
-import java.util.UUID;
+import java.time.Instant;
+import java.util.List;
 
-@RestController
-@RequestMapping("/api/users")
+@Controller
+@RequestMapping("/web/user")
 public class UserController {
 
     @Autowired
-    private UserDAOImpl userDao;
+    private UserDAOImpl userDAO;
 
-    @DeleteMapping("/delete/{id}")
-    public void deleteUser(@PathVariable int id){
-        UserDTO userDTO = userDao.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
-                "No user with this ID could be found"));
-        userDao.delete(userDTO);
-    }
-
-
-    @PostMapping("/create")
-    public ResponseEntity<String> createUser(@RequestBody LoginDTO login)
-    {
-        UserDTO newUser = new UserDTO();
-        UUID uuid = UUID.randomUUID(); //Generates random UUID
-        newUser.setEmail(login.getEmail());
-        newUser.setPassword(login.getPassword());
-        newUser.setKey(String.valueOf(uuid));
-        userDao.save(newUser);
-        return new ResponseEntity<>("User created successfully with email "
-                + newUser.getEmail()
-                + " and the generated API key is "
-                + newUser.getKey(), HttpStatus.OK);
-    }
-
-    @GetMapping("/login")
-    public ResponseEntity<String> loginUser(@RequestBody LoginDTO login) {
-        Optional<UserDTO> user = userDao.findByEmail(login.getEmail());
-        if(user.isPresent()){
-            if(login.getEmail().equals(user.get().getEmail()) && login.getPassword().equals(user.get().getPassword())){
-                return new ResponseEntity<>(user.get().getKey(), HttpStatus.OK);
-            } else {
-                return new ResponseEntity<>(
-                        "{\"message\":\"Invalid credentials!\"}", HttpStatus.NOT_FOUND);
-            }
-        }
-        return new ResponseEntity<>("Account does not exist.", HttpStatus.NOT_FOUND);
-    }
-
-    @PutMapping("/update")
-    public ResponseEntity<UserDTO> updateEmployeePut(@RequestBody UserDTO newState) {
-        Optional<UserDTO> original = userDao.findById(newState.getId());
-        if (original.isPresent()) {
-            userDao.updateUser(newState.getId(), newState);
-
-            return new ResponseEntity<>(userDao.findById(newState.getId()).get(), HttpStatus.OK);
-        }
-        else {
-            userDao.save(newState);
-            return new ResponseEntity<>(userDao.findById(newState.getId()).get(), HttpStatus.OK);
-        }
-
-    }
+    // --------------------------------- basic CRUD -------------------------------------------
     @GetMapping("/{id}")
-    public ResponseEntity<String> findById(@PathVariable int id){
-        Optional<UserDTO> result = userDao.findById(id);
-        ObjectMapper userMapper = JsonMapper.builder().addModule(new JavaTimeModule()).build();
-        ResponseEntity<String> returnValue = null;
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("content-type", "application/json");
-        if (result.isPresent()){
-            UserDTO userDTO = result.get();
-            String userString = null;
-            try {
-                userString = userMapper.writeValueAsString(userDTO);
-
-            } catch (JsonProcessingException e) {
-                throw new RuntimeException(e);
-            }
-            returnValue = new ResponseEntity<>(userString, headers, HttpStatus.OK);
-        } else {
-            returnValue = new ResponseEntity<>(
-                    "{\"message\":\"User " + id + " not found!\"}", headers, HttpStatus.NOT_FOUND
-            );
-        }
-        return returnValue;
+    public String getUser(@PathVariable("id") long id, Model model){
+        UserDTO user = userDAO.findById(id).orElse(null);
+        model.addAttribute("user", user);
+        return "userDisplay";
     }
 
 
