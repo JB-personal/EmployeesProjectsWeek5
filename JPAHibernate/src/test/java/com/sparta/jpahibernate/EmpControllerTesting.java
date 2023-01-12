9895a99ade8556e70e3fda85eac7cb9e65a39731
yash jagani <shi7su@protonmail.ch>
2023-01-12 18:06:24 +0000

    css,html,auth,queries

--- JPAHibernate/src/test/java/com/sparta/jpahibernate/EmpControllerTesting.java
+++ JPAHibernate/src/test/java/com/sparta/jpahibernate/EmpControllerTesting.java
@@ -1,9 +1,21 @@
 package com.sparta.jpahibernate;
 
+import com.sparta.jpahibernate.controllers.EmployeeController;
 import com.sparta.jpahibernate.dao.concretes.EmployeeDAOImpl;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClients;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.assertj.core.api.Assertions.assertThat;
+import java.io.IOException;
 
 @SpringBootTest
 public class EmpControllerTesting {
@@ -11,14 +23,25 @@ public class EmpControllerTesting {
     @Autowired
     private EmployeeDAOImpl dao;
 
+
+    @MockBean
+    private EmployeeController emp;
     // GET METHODS
 
-//    @Test
-//
-//    @Test
-//
-//    @Test
-//
+    @Test
+    public void getEmployee123() throws ClientProtocolException, IOException {
+
+    }
+    @Test
+    public void getAllEmployeeSuccess() throws ClientProtocolException, IOException {
+
+
+    }
+    @Test
+    public void getEmployeeFail() throws ClientProtocolException, IOException {
+
+
+    }
 //    @Test
 //
 //    @Test
